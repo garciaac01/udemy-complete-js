9938d5d9f81a6f31c06ba19c6e1626b04f98368a
garciaac01 <garciaac01@gmail.com>
2020-03-27 21:25:24 -0400

    wrap event listeners in function; add init function

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -31,28 +31,37 @@ var UIController = (function () {
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        // Adding the Enter keypress to the global document
+        // param automatically gets passed into event listener functions
+        document.addEventListener('keypress', function (event) {
+            // Only run if the enter key was pressed
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
 
     var ctrlAddItem = function () {
         // 1. Get the field input data
         var input = UICtrl.getInput();
-        console.log(input);
         // 2. Add item to the budget controller
         // 3. Add the new item to the UI
         // 4. Calculate the budget
         // 5. Display the button on the UI
     };
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    // Adding the Enter keypress to the global document
-    // param automatically gets passed into event listener functions
-    document.addEventListener('keypress', function (event) {
-        // Only run if the enter key was pressed
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function () {
+            console.log('Application has started.');
+            setupEventListeners();
         }
-    });
-
+    }
 
 })(budgetController, UIController);
+
+controller.init();
